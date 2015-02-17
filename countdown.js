a7d8fc462c8e23b1a9b3d95eaccf4f43691f8ecd
Alex Bickford <a.bickford@paltd.net>
2015-02-17 16:58:41 +0000

    Got the timer to run properly

--- countdown.js
+++ countdown.js
@@ -1,31 +1,35 @@
-//Set Static Variables
-var time_now = new Date();
-var ms_per_day = 24*60*60*1000;
-var text = "";
-var next_year = new Date().getFullYear() + 1;
+var countdown = function() {
 
-// User defined variables
+    //Set Static Variables
+    var time_now = new Date();
+    var ms_per_day = 24*60*60*1000;
+    var text = "";
+    var next_year = new Date().getFullYear() + 1;
 
-//Elements
-var appender = document.getElementById("appender"); //Where text will append to
-var append = document.createElement("span");
-var countdown = document.getElementById("countdown"); // Where countdown will update
+    // User defined variables
 
-//Times
-var timezone = "GMT+0000"; //What timezone
-var time_to_end = "16:00:00 " + timezone; //Time in day you want to count down to
-var count_to = new Date(time_now.getDate() + " " + time_now.getMonth() + " " + time_now.getFullYear() + " " + time_to_end); //The full day and time in a variable
+    //Elements
+    var appender = document.getElementById("appender"); //Where text will append to
+    console.log(appender + "\n\r" + document.getElementById("appender"));
+    var countdown = document.createElement("span"); // Where countdown will update
 
-//Holiday Variable
-holiday_start_date = new Date("12 22 2015"); //mm dd yyyy
-holiday_end_date   = new Date("1 05 2016");  //m dd yyyy
+    //Times
+    var timezone = "GMT+0000"; //What timezone
+    var time_to_end = "16:00:00 " + timezone; //Time in day you want to count down to
+    var count_to = new Date(time_now.getMonth() + " " + time_now.getDate() + ", " + time_now.getFullYear() + " " + time_to_end); //The full day and time in a variable
 
-function countdown() {
+    //Holiday Variable
+    holiday_start_date = new Date("12 22 2015"); //mm dd yyyy
+    holiday_end_date   = new Date("1 05 2016");  //m dd yyyy
+
+    /***************************************
+    *****DONT TOUCH ANYTHING UNDER HERE*****
+    ***************************************/
 
     //Create the countdown span
-    appender.parentNode.insertBefore(append, appender.nextSibling);
-    append.id = "countdown";
-    append.className += "ribbon block";
+    appender.parentNode.insertBefore(countdown, appender.nextSibling);
+    countdown.id = "countdown";
+    countdown.className += "ribbon block";
 
     if( time_now.getDay() == 5 || time_now.getDay() == 6) {
         return; //End it there, it's the weekend!
@@ -33,7 +37,7 @@ function countdown() {
 
     window.setInterval(function() {
         //Variables need to be calculated each second
-        var time_left = time_to_end.getTime() - now.getTime(),
+        var time_left = count_to.getTime() - new Date().getTime(),
             days_left = time_left / ms_per_day,
             m_days_left = Math.floor(days_left),
             hours_left = (days_left - m_days_left)*24,
@@ -43,7 +47,7 @@ function countdown() {
             seconds_left = (mins_left - m_mins_left)*60,
             m_seconds_left = Math.floor(seconds_left);
 
-        if (now >= holiday_start_date && now <= holiday_end_date) {
+        if (time_now >= holiday_start_date && time_now <= holiday_end_date) {
             text = "<i class='fa fa-clock-o dark-green'></i> We'll be back " + holiday_end_date;
         } else {
             text = "<i class='fa fa-clock-o dark-green'></i> Order within <span class='dark-green'>" + m_hours_left + "h : " + m_mins_left + "m : " + m_seconds_left + "s</span><br />for next working day delivery.";
@@ -56,7 +60,7 @@ function countdown() {
             countdown.innerHTML = text;
         }
 
-    }, 1000)
+    }, 1000);
 };
 
 countdown();
